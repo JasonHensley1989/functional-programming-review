0b853b58fe2105a812ef47d6ba45742eef0763ee
Jason Hensley <jasonhensley1989@gmail.com>
2023-02-24 14:56:47 -0600

    syntax issues fix

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ const numbers = [1, -1, 2, 44, 7, 6, -5];
 
 const filtered = numbers.filter(n => n >= 0);
 
-const items = filtered.map(n => {
-    return { value: n };   
-})
+const items = filtered.map(n => ({ value: n }));
 
 console.log(items)
