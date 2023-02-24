9046d890cbeab7e74617023a9faaaaa90580768d
Jason Hensley <jasonhensley1989@gmail.com>
2023-02-24 14:39:45 -0600

    filter/map  method

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ console.log("Javacript Active");
 
 // map method
 
-const numbers = [1, -1, 2, 4];
+const numbers = [1, -1, 2, -4];
 
 const filtered = numbers.filter(n => n >= 0);
 
-filtered.map()
+// filtered.map(n => "<li>" + )
 
+console.log(filtered)
