56d03d577cd9a5b57e9f89f9f460cdd4c598a2b2
Jason Hensley <jasonhensley1989@gmail.com>
2023-02-24 14:55:12 -0600

    iterate over filter using map

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ const numbers = [1, -1, 2, 44, 7, 6, -5];
 const filtered = numbers.filter(n => n >= 0);
 
 const items = filtered.map(n => {
-    const obj = { value: n };
-    return obj;
+    return { value: n };   
 })
 
 console.log(items)
