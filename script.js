e7aa74cf344ce15bd0a1ff6b5077f4752d05e250
Jason Hensley <jasonhensley1989@gmail.com>
2023-02-24 14:43:31 -0600

    filter/map/join  method

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ console.log("Javacript Active");
 
 // map method
 
-const numbers = [1, -1, 2, -4];
+const numbers = [1, -1, 2, 44, 7, 6, -5];
 
 const filtered = numbers.filter(n => n >= 0);
 
-// filtered.map(n => "<li>" + )
+const items = filtered.map(n => "<li>" + n + "</li>");
 
-console.log(filtered)
+const html = items.join()
+
+console.log(html)
