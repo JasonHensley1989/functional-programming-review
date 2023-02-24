600a0ea308225310c602aaf9cad80d3e674cf849
Jason Hensley <jasonhensley1989@gmail.com>
2023-02-24 15:01:49 -0600

    syntax issues fix

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ console.log("Javacript Active");
 
 // map method
 
-const numbers = [1, -1, 2, 44, 7, 6, -5];
+const numbers = [11, -1, 2, 44, 7, 6, -5];
 
-const filtered = numbers.filter(n => n >= 0);
+// method chaining
+const filtered = numbers.filter(n => n >= 0)
+.map(n => ({ value: n }))
+.filter(obj => obj.value  < 40)
+.map(obj => obj.value + "a")
 
-const items = filtered.map(n => ({ value: n }));
-
-console.log(items)
+ console.log(filtered)
